7573a867f225b3ef73efbc1c50c3ad4ab0e1af8f
Andy Phillipson <1063765+ajp8164@users.noreply.github.com>
2024-09-01 23:51:05 -0400

    Use env for count

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@
 import { Checkbox } from "./Checkbox";
 
 export default function Home() {
+  const count = process.env.COUNT || 1000;
+
   return (
     <main className={'flex flex-row flex-wrap'}>
-      {new Array(200000).fill(0).map((_v, i) => {
+      {new Array(count).fill(0).map((_v, i) => {
         return <Checkbox key={`${i}`} index={i} />
       })}
     </main>
